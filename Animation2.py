67fe064218a2aa2d124b6624ad43b3047ae315c1
danielffnando <daniel.f.m.soares@gmail.com>
2018-05-27 14:58:35 -0300

    Add files via upload

--- Animation2.py
+++ Animation2.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 
 fig, ax = plt.subplots()
 xsize, ysize, xarea, yarea = [], [], [], []
-ln1, = plt.plot([], [], 'ro', animated=True)
+ln, = plt.plot([], [], 'ro', animated=True)
 #ln2, = plt.plot([], [], 'ro', animated=True)
 xcord, ycord, area, time = [], [], [], []
 
@@ -16,22 +16,23 @@ for i in np.linspace(0, 20, 51):
     area.append(x*y)
     time.append(i)
 
-#plt.figure(num=1, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
-#plt.subplot(2, 1, 1)
-plt.plot(xcord, ycord)
-plt.title('Terreno')
+#plt.subplot(2, 1, 2)
+plt.plot(time, area)
+plt.title('Area')
+plt.xlabel('Time')
+plt.ylabel('Area')
 plt.grid(True)
 
 def init():
     ax.set_xlim(0, 22)
-    ax.set_ylim(0, 12)
-    return ln1,
+    ax.set_ylim(0, 52)
+    return ln,
 
 def update(frame):
     xsize = frame
-    ysize = -(frame)/2 + 10
-    ln1.set_data(xsize, ysize)
-    return ln1,
+    ysize = frame * (-frame/2 + 10)
+    ln.set_data(xsize, ysize)
+    return ln,
 
 
 ani = FuncAnimation(fig, update, frames=np.linspace(0, 20, 200),
